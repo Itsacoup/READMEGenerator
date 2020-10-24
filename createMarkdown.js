9bf045c01117e4ba245d7c29c84412034b1159e7
Matt Baber <mbaber1142@gmail.com>
2020-10-24 14:50:39 -0400

    first commit, index and package set

---
+++ createMarkdown.js
@@ -0,0 +1,10 @@
+
+function createMarkdown(userResponse) {
+    let contents = `Table of Contents`;
+
+    if (userResponse.ins)
+}
+
+
+
+module.exports = createMarkdown;
---
+++ index.js
@@ -0,0 +1,112 @@
+
+//modules required
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const createMarkdown = require("./createMarkdown.js")
+
+//questions to prompt user with inquirer
+const questions = [
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+        default: '',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: '',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+    type: 'input',
+        message: "If applicable, provide a licensing for your application. (view https://choosealicense.com/ for help",
+        name: 'license'
+    }    
+    
+];
+
+
+
+
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = createMarkdown(userResponses);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+// function call to initialize program
+init();
